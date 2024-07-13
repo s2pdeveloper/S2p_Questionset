01bcab6b358fc680c17cf8a09e67ec95a16fc5c2
s2pdeveloper <124123282+s2pdeveloper@users.noreply.github.com>
2024-07-13 19:25:03 +0530

    changes

--- server/src/controllers/v1/admin/seminar/routes.js
+++ server/src/controllers/v1/admin/seminar/routes.js
@@ -4,11 +4,13 @@ const seminaryController = require('./seminar');
 const { validate } = require('../../../../middleware/Validators');
 const AuthHelper = require('../../../../models/helpers/AuthHelper');
 
+app.get('/proxy', seminaryController.proxy);
 app.get('/', seminaryController.getAll);
 app.get('/list', seminaryController.getList);
 app.get('/:id', seminaryController.getById);
 app.post('/', seminaryController.create);
 app.put('/:id', seminaryController.update);
 app.delete('/:id', seminaryController.delete);
+
 
 module.exports = app;
