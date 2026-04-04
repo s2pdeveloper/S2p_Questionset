d18eac6df1065ec90ac47fb23104652ae948224c
mayank <murkutemayank@gmail.com>
2026-04-04 13:22:35 +0530

    changes done

--- admin-panel/src/environments/environment.prod.ts
+++ admin-panel/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'http://109.106.255.69:1988/api/v1/admin/',
-  QrCodeUrl:"http://109.106.255.69:1989/#/",
+  apiEndpoint: 'http://72.61.125.250:1989/api/v1/admin/',
+  QrCodeUrl:"http://72.61.125.250:1989/#/",
 
   // apiEndpoint: './api/v1/admin/',
   //  QrCodeUrl:" http://localhost:1988"
--- admin-panel/src/environments/environment.ts
+++ admin-panel/src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  apiEndpoint: 'http://109.106.255.69:1988/api/v1/admin/',
-  QrCodeUrl:"http://109.106.255.69:1989/#/",
+  apiEndpoint: 'http://72.61.125.250:1989/api/v1/admin/',
+  QrCodeUrl:"http://72.61.125.250:1989/#/",
 
   // apiEndpoint: 'http://localhost:1988/api/v1/admin/',
   // QrCodeUrl:"http://localhost:1988"
