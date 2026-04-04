04349b4b641332f6c0c2d1d4b306d3cbdaab9a5d
mayank <murkutemayank@gmail.com>
2026-04-04 13:21:01 +0530

    deploy

--- website/src/environment/environment.ts
+++ website/src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiEndpoint: 'http://109.106.255.69:1988/api/v1/website/',  
+    apiEndpoint: 'http://72.61.125.250:1989//api/v1/website/',  
     //  apiEndpoint: 'http://localhost:1988/api/v1/website/',
   };
